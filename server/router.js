cba69fa3fae168edbb5b9cbc0007efebfca7a3e5
Avatadahmad <attarad@mail.uc.edu>
2020-10-23 17:53:31 -0400

    feat: Stable working code

---
+++ server/router.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+
+let router = express.Router();
+
+router.get('/:movie',(req,res)=>{
+    const movie = req.params.movie
+    const url = `http://www.omdbapi.com/?s=${movie}&apikey=52ab89f7`;
+    console.log(url)
+    fetch(url)
+        .then(response =>response.json())
+        .then(data =>{
+            console.log("data:",data);
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.send(data)
+
+            
+        })
+        
+        .catch(err =>{
+            console.log(err);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+        
+})
+
+router.get('/:x',(req,res)=>{
+    const movie_id = req.params.x
+    const url = `http://www.omdbapi.com/?i=${movie_id}&apikey=52ab89f7`;
+    console.log(url)
+    fetch(url)
+        .then(response =>response.json())
+        .then(data =>{
+            console.log("data:",data);
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.send(data)
+
+            
+        })
+        
+        .catch(err =>{
+            console.log(err);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+        
+})
+module.exports = router;
